1e63516f895f28bfb7af2463c41ff973b4bfdaa6
orange <tenghoit.kouch@centre.edu>
2024-09-18 17:28:54 +0000

    added lab2b sql file

--- lab3/lab03_sol.sql
+++ lab3/lab03_sol.sql
@@ -13,5 +13,23 @@ CREATE TABLE movies (
 )
 
 CREATE TABLE consumers (
-    
+    consumers_consumer_id INT AUTO_INCREMENT,
+    consumers_first_name VARCHAR(64),
+    consumers_last_name VARCHAR(64),
+    consumers_address VARCHAR(128),
+    consumers_city VARCHAR(64),
+    consumers_state VARCHAR(2),
+    consumers_zip_code VARCHAR(5),
+    PRIMARY KEY (consumers_consumer_id)
+)
+
+CREATE TABLE ratings (
+    ratings_movie_id INT,
+    ratings_consumer_id INT,
+    ratings_when_dated DATETIME,
+    ratings_number_stars INT,
+    FOREIGN KEY (ratings_movie_id) REFERENCES movies(movies_movie_id),
+    FOREIGN KEY (ratings_consumer_id) REFERENCES consumers(consumers_consumer_id),
+    PRIMARY KEY (ratings_movie_id, ratings_consumer_id)
+
 )

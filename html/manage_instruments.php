7072eaaebe5df27cb418f9e5d3ca0cce0f137605
orange <tenghoit.kouch@centre.edu>
2024-11-01 02:15:44 +0000

    finished lab8

--- html/manage_instruments.php
+++ html/manage_instruments.php
@@ -9,7 +9,6 @@
 ?>
 
 <?php
-
     function result_to_html_table($result) {
         $qryres = $result->fetch_all();
         $n_rows = $result->num_rows;
@@ -21,7 +20,7 @@
         
         <!-- Begin header - - - - - - - - - - - - - - - - - - - - -->
         <!-- Using default action (this page). -->
-         <form action="POST">
+         <form method="POST">
             <table>
                 <thead>
                     <tr>
@@ -47,7 +46,72 @@
             </table>
             <p><input type="submit" name="delbtn" value="Delete Selected Records" /></p>
         </form>
-    <?php } 
+<?php } ?>
+
+<?php
+    $config = parse_ini_file('/home/tenghoitkouch/mysql.ini');
+    $sql_location = "/home/tenghoitkouch/csc362_f24_kouch/html/";
+    $dbname = 'instrument_rentals';
+    $conn = new mysqli(
+                $config['mysqli.default_host'],
+                $config['mysqli.default_user'],
+                $config['mysqli.default_pw'],
+                $dbname);
+
+    if ($conn->connect_errno) {
+        echo "Error: Failed to make a MySQL connection, here is why: ". "<br>";
+        echo "Errno: " . $conn->connect_errno . "\n";
+        echo "Error: " . $conn->connect_error . "\n";
+        exit; // Quit this PHP script if the connection fails.
+    } 
+
+    //add recs
+    if(array_key_exists('add_records', $_POST)){
+
+        $add_tbl = file_get_contents($sql_location . 'add_instruments.sql');
+        // $add_stmt = $conn->prepare($add_tbl);
+        // $add_stmt->execute();
+        if(!$conn->query($add_tbl)){
+            echo $conn->error;
+            echo "Failed to insert records!\n";
+        } else {
+            header("Location: {$_SERVER['REQUEST_URI']}", true, 303);
+            exit(); // In this case, we are GETting the page we currently on.
+        }
+    }
+
+    //select records
+    $sel_stmt = file_get_contents($sql_location . "select_instruments.sql");
+    $result = $conn->query($sel_stmt);
+    echo $result->field_count . " field(s) in results.<br>";
+    echo $result->num_rows . " row(s) in results.";
+    $instrument_list = $result->fetch_all();
+
+    $need_reload = FALSE;
+    //del rec
+    if(array_key_exists('delbtn', $_POST)){
+
+        $del_str = file_get_contents('/home/tenghoitkouch/csc362_f24_kouch/html/delete_instrument.sql');
+        $del_stmt = $conn->prepare($del_str);
+        $del_stmt->bind_param('i', $id);
+
+        // $get_all_instrument_ids = "SELECT instrument_id FROM instruments";
+        // $idlist = $conn->query($get_all_instrument_ids);
+
+        for($i = 0; $i < $result->num_rows; $i++){
+            $id = $instrument_list[$i][0];
+            if(array_key_exists('checkbox' . $id, $_POST)){
+                $need_reload = TRUE;
+                $del_stmt->execute();
+            }
+        }
+    }
+
+    // ----- Reload this page if the database was changed.
+    if($need_reload){ // This needs to be done before any output, to guarantee that it works.
+        header("Location: {$_SERVER['REQUEST_URI']}", true, 303);
+        exit();
+    }
 ?>
 
 <!DOCTYPE html>
@@ -59,65 +123,14 @@
 </head>
 <body>
     
-    <?php
-        $config = parse_ini_file('/home/tenghoitkouch/mysql.ini');
-        $dbname = 'instrument_rentals';
-        $conn = new mysqli(
-                    $config['mysqli.default_host'],
-                    $config['mysqli.default_user'],
-                    $config['mysqli.default_pw'],
-                    $dbname);
-
-        if ($conn->connect_errno) {
-            echo "Error: Failed to make a MySQL connection, here is why: ". "<br>";
-            echo "Errno: " . $conn->connect_errno . "\n";
-            echo "Error: " . $conn->connect_error . "\n";
-            exit; // Quit this PHP script if the connection fails.
-        } 
-        $sql_location = '/home/tenghoitkouch/csc362_f24_kouch/html/';
-        $sel_tbl = file_get_contents($sql_location . 'select_instruments.sql');
-        // echo $sel_tbl;
-        $result = $conn->query($sel_tbl);
-        echo $result->field_count . " field(s) in results.<br>";
-        echo $result->num_rows . " row(s) in results.";
+    <?php 
+        $result = $conn->query($sel_stmt);
         result_to_html_table($result);
-    ?>
-
-    <?php
-        if(array_key_exists('delbtn', $_POST)){
-
-            $get_all_instrument_ids = "SELECT instrument_id FROM instruments";
-            $idlist = $conn->query($get_all_instrument_ids);
-
-            for($i = 0; $i < count($idlist); $i++){
-                $id = $idlist[0];
-                if(in_array("checkbox" . $id, $_POST)){
-                    $del_stmt = file_get_contents('/home/tenghoitkouch/csc362_f24_kouch/html/delete_instrument.sql');
-                    $del_stmt = $conn->prepare("DELETE...");
-                    $del_stmt->bind_param('i', $id);
-                    $$del_stmt->execute();
-                }
-            }
-
-            header("Location: {$_SERVER['REQUEST_URI']}", true, 303);
-            exit();
-        }
     ?>
 
     <form method="POST">
         <input type="submit" name="add_records" value="Add extra records" />  
     </form>
 
-    <?php
-        if(array_key_exists('add_records', $_POST)){
-
-            $add_tbl = file_get_contents('/home/tenghoitkouch/csc362_f24_kouch/html/add_instruments.sql');
-            $add_stmt = $conn->prepare($add_tbl);
-            $add_stmt->execute();
-
-            header("Location: {$_SERVER['REQUEST_URI']}", true, 303);
-            exit();
-        }
-    ?>
 </body>
 </html>

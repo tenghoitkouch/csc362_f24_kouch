304ed7b9274fa2447edcc4fdc4662c94897e9f05
orange <tenghoit.kouch@centre.edu>
2024-11-12 15:46:07 +0000

    finished

--- lab10/pokemon_league_starter_code.sql
+++ lab10/pokemon_league_starter_code.sql
@@ -33,6 +33,24 @@ RETURN (
         GROUP BY (trainer_id)
 );
 
+DROP FUNCTION IF EXISTS get_trainer_id;
+CREATE FUNCTION get_trainer_id(pokemon_id_input INT)
+RETURNS INT
+RETURN (
+    SELECT trainer_id
+        FROM    pokemon
+        WHERE   pokemon_id = pokemon_id_input
+);
+
+DROP FUNCTION IF EXISTS get_party_status;
+CREATE FUNCTION get_party_status(pokemon_id_input INT)
+RETURNS INT
+RETURN (
+    SELECT pokemon_is_in_party
+        FROM    pokemon
+        WHERE   pokemon_id = pokemon_id_input
+);
+
 
 --range of 1 - 6 pokemon in party
 
@@ -80,8 +98,37 @@ END; $$
 DELIMITER ;
 
 
+DELIMITER $$
+CREATE PROCEDURE trade_pokemon (pokemon_id_input INT, trainer1_id_input INT, trainer2_id_input INT)
+BEGIN
+    -- MariaDB uses error 1644 for unhandled user-defined exceptions.
+    -- This makes sense when an error is not intended to be caught, in order to 
+    -- prevent some action from occurring.
+    DECLARE EXIT HANDLER FOR 45000
+    BEGIN
+    SELECT 'Procedure failed because of reasons!' as Error;
+        ROLLBACK;
+    END;
 
+    -- <> is the not operator
+    IF (get_trainer_id(pokemon_id_input) <> trainer1_id_input) THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Pokemon not owned by trainer 1!';
+    END IF;
 
+    IF (get_party_status(pokemon_id_input) = FALSE) THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Pokemon not currently in party!';
+    END IF;
+
+    START TRANSACTION;
+        
+        UPDATE  pokemon
+        SET     trainer_id = trainer2_id_input
+        WHERE   pokemon_id = pokemon_id_input;
+
+    COMMIT;
+
+END; $$
+DELIMITER ;
 
 
 INSERT INTO trainers (trainer_name, trainer_hometown)
@@ -105,7 +152,15 @@ INSERT INTO pokemon (pokemon_species, pokemon_level, trainer_id, pokemon_is_in_p
         ("Onyx",    "52", 1, TRUE),
         ("Magicarp","12", 1, TRUE),
         ("Pikachu", "58", 1, TRUE),
-        ("Staryu",  "44", 1, TRUE); 
+        ("Staryu",  "44", 1, TRUE),
+        ("Onyx",    "80", 1, FALSE);
+
+--data for trade
+INSERT INTO pokemon (pokemon_species, pokemon_level, trainer_id, pokemon_is_in_party)
+ VALUES ("Onyx",    "80", 1, FALSE);
+
+INSERT INTO pokemon (pokemon_species, pokemon_level, trainer_id, pokemon_is_in_party)
+ VALUES ("Magicarp","12", 1, TRUE);
 
 --test for del
 DELETE FROM pokemon
@@ -116,3 +171,11 @@ UPDATE pokemon
 SET     trainer_id = 1
 WHERE   pokemon_id = 3;
 
+--test trade: 
+SELECT * FROM pokemon;
+
+CALL trade_pokemon (12, 2, 3); -- should fail, not owned by trainer 2
+CALL trade_pokemon (12, 1, 3); -- should fail, not in party
+CALL trade_pokemon (13, 1, 3); -- should work
+
+SELECT * FROM pokemon;
